eaf7de7547dd54395c2ad3d108e90f559f927f01
Heidi Rajala <heidi.rajala@integrify.io>
2023-09-07 21:45:42 +0300

    Read and write from own storage container

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Azure.Storage.Blobs;
 
 namespace Blobpics.Pages;
 
+[BindProperties]
 public class IndexModel : PageModel
 {
+    private IConfiguration _configuration;
+    private string _connectionString;
+    private const string _containerName = "kissakuvakontti";
+    private BlobContainerClient _containerClient;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public List<string> ImageUrls { get; set; } = new List<string>();
+
+    public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
     {
+        _configuration = configuration;
         _logger = logger;
+        _connectionString = _configuration.GetConnectionString("AzureStorage");
+
+        _containerClient = new BlobContainerClient(_connectionString, _containerName);
+        _containerClient.CreateIfNotExistsAsync().Wait();
     }
 
     public void OnGet()
     {
+        var blobItems = _containerClient.GetBlobs();
+        foreach (var blobItem in blobItems)
+        {
+            var url = _containerClient.GetBlobClient(blobItem.Name).Uri.ToString();
+            ImageUrls.Add(url);
+        }
+    }
 
+    public async Task<IActionResult> OnPost(List<IFormFile> photos)
+    {
+        foreach (var photo in photos)
+        {
+            var blobClient = _containerClient.GetBlobClient(photo.FileName);
+            if (!blobClient.Exists())
+            {
+                using var stream = new MemoryStream();
+                await photo.CopyToAsync(stream);
+                stream.Position = 0;
+                await blobClient.UploadAsync(stream);
+            }
+        }
+        return RedirectToPage();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Blobpic
+
+## Description
+
+This is an azure and c# excercise project for blob things.
+It is the first time I used C# in front-end code.
